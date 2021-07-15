1416a7daab17621255d848eb42141380afe01379
tokarev <tokarev@kg.ru>
2021-07-15 22:07:07 +0300

    Changing methods

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from bot_manager import BotManager
 from utils import logging
 
-bot_manager = BotManager()
-API_TOKEN = bot_manager.get_token()
+API_TOKEN = BotManager().get_token()
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
@@ -16,9 +15,39 @@ dp = Dispatcher(bot)
 async def send_welcome(message: types.message):
     current_user = message.from_user
     logging.info(f'Бот поздоровался с пользователем: {current_user.mention, current_user.id}')
-    if current_user.locale.language != 'ru':
-        await message.reply("Hi!\nI'm Bot!\nPowered by aiogram.")
-    await message.reply("Привет!\nЯ бот\nРаботаю на aiogramm.")
+    if current_user.language_code != 'ru':
+        await message.answer("Hi!\n"
+                             "I'm Bot!\n"
+                             "Powered by aiogram.")
+
+    await message.answer(f'Привет\n'
+                         f'Теперь я твой ежедневник\n'
+                         f'Чтобы посмотреть список комманд используй: /help.')
+
+
+@dp.message_handler(commands=['help'])
+async def help_commands(message: types.message):
+    await message.answer(f'(В разработке)Чтобы добавить запись о событии используйте: /add_event\n'
+                         f'(В разработке)Чтобы посмотреть все записи за все время используйте: /show_all_events\n'
+                         f'(В разработке)Чтобы создать напоминание о событии используйте: /add_reminder')
+
+
+@dp.middleware
+
+
+@dp.message_handler(commands=['add_event'])
+async def add_event(message: types.message):
+    await message.answer('В разработке')
+
+
+@dp.message_handler(commands=['show_all_events'])
+async def add_event(message: types.message):
+    await message.answer('В разработке')
+
+
+@dp.message_handler(commands=['add_reminder'])
+async def add_event(message: types.message):
+    await message.answer('В разработке')
 
 
 def main():
