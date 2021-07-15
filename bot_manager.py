8c5a142f2f9432110da262797dea3c69cbfe55da
tokarev <tokarev@kg.ru>
2021-07-15 22:06:17 +0300

    Using pattern Singleton

--- bot_manager.py
+++ bot_manager.py
@@ -1,9 +1,18 @@
+"""Содержит базовую реализацию менеджера бота при помощи паттерна Одиночка"""
 import logging
 from configparser import ConfigParser
 
 
-class BotManager:
+class BotManagerMeta(type):
+    _instances = None
 
+    def __call__(cls, *args, **kwargs):
+        if cls._instances is None:
+            cls._instances = super().__call__(*args, **kwargs)
+        return cls._instances
+
+
+class BotManager(metaclass=BotManagerMeta):
     def __init__(self):
         self.bot_token = None
 
@@ -19,6 +28,3 @@ class BotManager:
 
     def set_token(self, config):
         self.bot_token = config['DATA']['TOKEN']
-
-
-
