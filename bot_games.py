ac928ef1a9452d3404b0846f098a4e6090e44ff7
tokarev <tokarev@kg.ru>
2021-07-18 14:51:06 +0300

    Created a game

---
+++ bot_games.py
@@ -0,0 +1,26 @@
+from asyncio import sleep
+
+from utils import logging
+
+
+class DiceGame:
+    @staticmethod
+    async def start_game(bot, message):
+        current_user = message.from_user
+        logging.info(f'Пользователь: {current_user.mention, current_user.id}, решил поиграть в Dice')
+        await message.answer('Начинаем игру!')
+        await sleep(2)
+        await message.answer('Бот кидает кубик!')
+        await sleep(1)
+        bot_dice = await bot.send_dice(message.chat.id, allow_sending_without_reply=True)
+        await sleep(5)
+        await message.answer('Вы кидаете кубик!')
+        await sleep(1)
+        user_dice = await bot.send_dice(message.chat.id, allow_sending_without_reply=True)
+        await sleep(5)
+        if bot_dice['dice']['value'] > user_dice['dice']['value']:
+            await message.answer('Вы проиграли!')
+        elif bot_dice['dice']['value'] < user_dice['dice']['value']:
+            await message.answer('Вы победили!')
+        else:
+            await message.answer('Ничья')
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import asyncio
 import platform
+from asyncio import sleep
 
 from aiogram import Bot, Dispatcher, executor, types
 
 from bot_manager import BotManager
+from bot_games import DiceGame
 from utils import logging
 
 API_TOKEN = BotManager().get_token()
@@ -18,21 +21,19 @@ async def send_welcome(message: types.message):
     if current_user.language_code != 'ru':
         await message.answer("Hi!\n"
                              "I'm Bot!\n"
-                             "Powered by aiogram.")
+                             "All commands /help")
 
     await message.answer(f'Привет\n'
                          f'Теперь я твой ежедневник\n'
-                         f'Чтобы посмотреть список комманд используй: /help.')
+                         f'Чтобы посмотреть список комманд используй: /help')
 
 
 @dp.message_handler(commands=['help'])
 async def help_commands(message: types.message):
     await message.answer(f'(В разработке)Чтобы добавить запись о событии используйте: /add_event\n'
                          f'(В разработке)Чтобы посмотреть все записи за все время используйте: /show_all_events\n'
-                         f'(В разработке)Чтобы создать напоминание о событии используйте: /add_reminder')
-
-
-@dp.middleware
+                         f'(В разработке)Чтобы создать напоминание о событии используйте: /add_reminder\n'
+                         f'Чтобы поиграть с ботом в кости используйте: /dice_game')
 
 
 @dp.message_handler(commands=['add_event'])
@@ -48,6 +49,11 @@ async def add_event(message: types.message):
 @dp.message_handler(commands=['add_reminder'])
 async def add_event(message: types.message):
     await message.answer('В разработке')
+
+
+@dp.message_handler(commands=['dice_game'])
+async def dice_game(message: types.message):
+    await asyncio.gather(DiceGame.start_game(bot, message))
 
 
 def main():
