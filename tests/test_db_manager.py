7d77e63f30bce94ad806541e68e12ba75ded71be
tokarev <tokarev@kg.ru>
2021-07-19 18:39:11 +0300

    Added a db manager test connection

---
+++ tests/test_db_manager.py
@@ -0,0 +1,13 @@
+import unittest
+
+from db_manager import DbManager
+
+
+class TestDbManager(unittest.TestCase):
+    def test_connection(self):
+        db = DbManager()
+        self.assertTrue(db._connection)
+
+
+if __name__ == '__main__':
+    unittest.main()
